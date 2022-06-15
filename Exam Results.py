5f7acfe688f036cd29a46bfd2b892dbebe36f665
wiekusviljoen <wiekusviljoen@gmail.com>
2022-06-15 16:48:39 +0200

    first commit

---
+++ Exam Results.py
@@ -0,0 +1,17 @@
+score = int(input("Score:"))
+
+if score >= 90 and score <= 100:
+    print("Grade Results: A")
+
+elif score >= 80 and score <= 90:
+    print("Grade Results: B ")
+
+
+elif score >= 70 and score <= 80:
+    print("Grade Results: C ")
+
+elif score >= 60 and score < 70:
+    print("Grade Results: D ")
+
+else:
+    print("Sorry You Failed the Exam")
